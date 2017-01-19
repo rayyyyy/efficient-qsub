888743a06f7eacae0a4eda34cf0da0af958e7afd
rayyyyy <ant_zxl@sina.com>
2017-01-19 11:02:00 +0800

    1st upload
    Different name means different operation.These codes are able to get rid of repeatable operations in bash or ssh.

---
+++ cnt_static_qsub.py
@@ -0,0 +1,25 @@
+import os
+
+dir_name1 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/16/isolated/caledinSHNU/static'
+os.chdir(dir_name1)
+os.system('qsub vasp.pbs')
+
+dir_name2 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/17/isolated/caledinSHNU/static'
+os.chdir(dir_name2)
+os.system('qsub vasp.pbs')
+
+dir_name3 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/18/isolated/caledinSHNU/static'
+os.chdir(dir_name3)
+os.system('qsub vasp.pbs')
+
+dir_name4 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/19/isolated/caledinSHNU/static'
+os.chdir(dir_name4)
+os.system('qsub vasp.pbs')
+
+dir_name5 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/19/isolated/caledinSHNU/static'
+os.chdir(dir_name5)
+os.system('qsub vasp.pbs')
+
+dir_name7 = '/home/users/xiangye/fzxl/carbonnanotube/cnt99/2tube/25/isolated/caledinSHNU/static'
+os.chdir(dir_name7)
+os.system('qsub vasp.pbs')
